dcfde2a5ae139a2974d1fd48ab24526b139fdcd9
Neel Shah <neshah@vehicle-vpn.emrcp.net>
2016-03-21 15:19:29 -0700

    working data capture

--- datagen.py
+++ datagen.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 
-import time, sys, os.path
+import time, sys, os.path, math
 
+temp = []
 dataType = 0 # 0 - time, 1 - solution, 2 - SV
 dataSet = []
-dataPt = []
+dataPt = [0]*10
 satData = []
 satData.append([])
 satData.append([])
@@ -13,13 +14,10 @@ if not os.path.isfile(sys.argv[1]):
 	exit("Could not find file")
 with open(sys.argv[1]) as file:
 	for line in file:
-#		print line
 		if 'Gps Solution' in line and dataType == 0:
 			dataType = 1
-			continue
-		elif 'Healthy DGPS' in line and dataType == 1:
+		elif 'Healthy\tDGPS' in line and dataType == 1:
 			dataType = 2
-			continue
 		elif '----------------------------------------------------' in line and dataType == 2:
 			# assemble data line here
 			dataPt[8] = sum(satData[1])/len(satData[1])
@@ -29,14 +27,12 @@ with open(sys.argv[1]) as file:
 			satData = []
 			satData.append([])
 			satData.append([])
-			continue
 
 		if dataType == 0:
 			try:
 				dataPt[0] = time.strptime(line,"%a %b %d %H:%M:%S %Z %Y\n")
-				print 'h'
 			except:
-				continue #print "time capture exception"
+				continue
 		elif dataType == 1:
 			try:
 				if "Sats visible" in line:
@@ -48,24 +44,21 @@ with open(sys.argv[1]) as file:
 				elif "Longitude" in line:
 					dataPt[2] = float(line.split(':')[1].split(' ')[1])
 				elif "Pos Acc" in line:
-					temp = x.split(':')[1].split(' ')
+					temp = line.split(':')[1].split(' ')
 					del temp[0::2]
 					dataPt[5] = math.sqrt(sum(map(lambda i:i*i,[float(j) for j in temp])))
 				elif "Vel Acc" in line:
-					dataPt[6] = float(x.split(':')[1].split(' ')[1])
+					dataPt[6] = float(line.split(':')[1].split(' ')[1])
 				elif "Time Acc" in line:
-					dataPt[7] = float(x.split(':')[1].split(' ')[1])
+					dataPt[7] = float(line.split(':')[1].split(' ')[1])
 			except:
-				continue #print "gps capture exception"
+				continue			
 		elif dataType == 2:
 			try:
 				# read in tab delimited values here
-				y = x.replace(' ','').split('\t')
+				y = line.replace(' ','').split('\t')
 				if (y[4] == 'Y'):
 					satData[0].append(int(y[1]))
 					satData[1].append(float(y[2]))
 			except:
-				continue #print "sv capture exception"
-
-print dataSet
-print dataPt
+				continue
